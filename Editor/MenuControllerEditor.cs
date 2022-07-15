8f4bdc6f187132efb3bc06c9dc8d5258c4532165
Nikita <109090771+Bloknoter@users.noreply.github.com>
2022-07-15 15:49:48 +0200

    Outlook update and page name validation
    New outlook of the component. It bceame more user-friendly. Also added convenient icons that detect issues with the pages. Added validation of unique page name

--- Editor/MenuControllerEditor.cs
+++ Editor/MenuControllerEditor.cs
@@ -10,9 +10,14 @@ namespace MenuEngine.EditorScripts
     [CanEditMultipleObjects]
     public class MenuControllerEditor : Editor
     {
+        private const string PAGE_FIELD_NAME = "Set name";
+
         private SerializedProperty StartPageName;
 
         private SerializedProperty pages;
+
+        private string focusedTextField = "";
+        private string previousValue = "";
 
         private GUIContent deleteIcon;
         private GUIContent addIcon;
@@ -21,6 +26,8 @@ namespace MenuEngine.EditorScripts
         private GUIContent errorIcon;
         private GUIContent warningIcon;
         private GUIContent transitionIcon;
+
+        private GUIContent emptyContent;
 
         private void OnEnable()
         {
@@ -40,14 +47,13 @@ namespace MenuEngine.EditorScripts
             warningIcon = EditorGUIUtility.IconContent("console.warnicon.sml");
             warningIcon.tooltip = "fix warnings!";
             transitionIcon = EditorGUIUtility.IconContent("d_tab_next");
-            //"d_forward"
-                
+            emptyContent = new GUIContent();
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
-            EditorGUILayout.HelpBox("The work with the names of pages does not depends on the string case and the spaces. \n \nBe carefull! Every name must be unique!", MessageType.Info);
+            EditorGUILayout.HelpBox("Page handler is not case-sensitive and space-sensitive! \nEvery name must be unique!", MessageType.Info);
             EditorGUILayout.Separator();
             StartPageName.stringValue = EditorGUILayout.TextField("Start page", StartPageName.stringValue);
 
@@ -67,25 +73,31 @@ namespace MenuEngine.EditorScripts
 
             if (pages.arraySize > 0)
             {
-                
+                Dictionary<string, int> names = new Dictionary<string, int>();
                 for(int i = 0; i < pages.arraySize;i++)
                 {
                     EditorGUILayout.BeginVertical("Tooltip");
                     SerializedProperty page = pages.GetArrayElementAtIndex(i);
                     SerializedProperty Name = page.FindPropertyRelative("Name");
+                    string pageName = Name.stringValue;
+                    
                     SerializedProperty pageobject = page.FindPropertyRelative("PageObject");
 
                     EditorGUILayout.BeginHorizontal();
                     Rect rect = EditorGUILayout.GetControlRect();
                     rect.x += 20;
                     rect.width -= 30;
-                    if (Name.stringValue == "")
-                        page.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(rect, page.isExpanded, $"Page {i + 1}", EditorStyles.popup);
+                    if (pageName == "")
+                        page.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(rect, page.isExpanded, $"<Empty name>", EditorStyles.popup);
                     else
-                        page.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(rect, page.isExpanded, $"{Name.stringValue}", EditorStyles.popup);
-                    if(Name.stringValue == "")
+                        page.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(rect, page.isExpanded, $"{pageName}", EditorStyles.popup);
+                    if (pageName == "" || names.ContainsKey(pageName.ToLowerInvariant().Replace(" ", "")))
                     {
                         EditorGUILayout.LabelField(warningIcon, GUILayout.Width(20));
+                    }
+                    else
+                    {
+                        EditorGUILayout.LabelField(emptyContent, GUILayout.Width(20));
                     }
                     if (pageobject.objectReferenceValue == null)
                     {
@@ -101,14 +113,18 @@ namespace MenuEngine.EditorScripts
                     EditorGUILayout.EndHorizontal();
                     if (page.isExpanded)
                     {
-                        EditorGUILayout.PropertyField(Name);
-                       
-                        if (Name.stringValue == "")
+                        GUI.SetNextControlName(PAGE_FIELD_NAME + $"{i}");
+                        Name.stringValue =  EditorGUILayout.TextField("Name", Name.stringValue);
+                        pageName = Name.stringValue;
+                        if (pageName == "")
                         {
                             EditorGUILayout.HelpBox("Name this page, or it won't work correctly", MessageType.Warning);
                         }
+                        if (names.ContainsKey(pageName))
+                        {
+                            EditorGUILayout.HelpBox("Page with this name already exists", MessageType.Warning);
+                        }
 
-                        
                         EditorGUILayout.PropertyField(page.FindPropertyRelative("PageObject"), new GUIContent("Page object"));
                         if (pageobject.objectReferenceValue == null)
                         {
@@ -131,7 +147,7 @@ namespace MenuEngine.EditorScripts
                                 rect.width -= 50;
                                 SerializedProperty transition = transitions.GetArrayElementAtIndex(t);
                                 SerializedProperty nextpage = transition.FindPropertyRelative("transition");
-                                transition.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(rect, transition.isExpanded, $"{Name.stringValue} -> {nextpage.stringValue}");
+                                transition.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(rect, transition.isExpanded, $"{pageName} -> {nextpage.stringValue}");
                                 EditorGUILayout.EndFoldoutHeaderGroup();
                                 if (GUILayout.Button(EditorGUIUtility.IconContent("d_TreeEditor.Trash"), GUILayout.Width(40)))
                                 {
@@ -148,7 +164,7 @@ namespace MenuEngine.EditorScripts
                                     for (int p = 0; p < pages.arraySize; p++)
                                     {
                                         SerializedProperty otherName = pages.GetArrayElementAtIndex(p).FindPropertyRelative("Name");
-                                        if (otherName.stringValue != "" && otherName.stringValue != Name.stringValue)
+                                        if (otherName.stringValue != "" && otherName.stringValue != pageName)
                                         {
                                             pagesnames.Add(otherName.stringValue);
                                             if (nextpage.stringValue != "" && nextpage.stringValue == otherName.stringValue)
@@ -189,6 +205,9 @@ namespace MenuEngine.EditorScripts
                         }
                         
                     }
+                    string pageNameValid = pageName.ToLowerInvariant().Replace(" ", "");
+                    if (!names.ContainsKey(pageNameValid))
+                        names.Add(pageNameValid, 0);
                     EditorGUILayout.EndVertical();
                     //EditorGUILayout.LabelField("____________________________________________________________________________");
                     EditorGUILayout.Separator();
